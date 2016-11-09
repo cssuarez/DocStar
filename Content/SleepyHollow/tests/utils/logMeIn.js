62b6ae93360906dc6dab4632612d40bb94c53823
cssuarez <lusoma@gmail.com>
2016-11-08 23:26:15 -0300

    DocStar Commit
    DocStar Commit

---
+++ Content/SleepyHollow/tests/utils/logMeIn.js
@@ -0,0 +1,14 @@
+﻿
+
+exports.logIn = function (callback) {
+    var func = function() {
+        test.assertExists('#userName, input[type="password"], #loginButton', 'All elements found');
+        this.fillSelectors('#addForm', {
+            '#userName': 'smoke@d.com',
+            'input[type="password"]': 'smoke'
+        }, false);
+        this.click('#loginButton');
+    }
+
+    return callback(func);
+}
---
+++ Content/SleepyHollow/tools/out/phantom/original-src/dev/scripts/myNumbers.js
@@ -0,0 +1,21 @@
+﻿define(function () {
+    'use strict';
+
+    var self = {};
+
+    self.add = function add() {
+        var operands = Array.prototype.slice.call(arguments),
+            total = 0;
+
+        for (var i = 0; i < operands.length; i++) {
+            if (typeof operands[i] === 'string') {
+                operands[i] = parseInt(operands[i], 10) || 0;
+            }
+            total += operands[i];
+        }
+
+        return total;
+    }
+
+    return self;
+});
